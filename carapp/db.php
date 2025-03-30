5c105743455624d66e1d3974a25acf571cc9fac6
wscott20 <cwsred2003@gmail.com>
2025-03-30 14:57:39 -0400

    fixed db.php

--- carapp/db.php
+++ carapp/db.php
@@ -1,6 +1,10 @@
- <?php
-    $mysqli = new mysqli('localhost', 'root', '', 'Cars' );
+<?php
+try {
+    $mysqli = new mysqli('sql111.infinityfree.com', 'if0_38310603', 'uBHu8fW6r5', 'if0_38310603_cars' );
     if (mysqli_connect_errno())
         die("Connect failed: %s\n".mysqli_connect_error());
-    //else echo 'Connected to mySQL<br>';
+    //echo 'Connected successfully to mySQL.<br>'; 
+} catch (Throwable $e) {
+    echo $e;
+}
 ?>
