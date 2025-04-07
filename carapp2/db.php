95c08e000467a65c54f2601b4e68e163ef68a69b
wscott20 <cwsred2003@gmail.com>
2025-04-07 08:19:57 -0400

    Updated db.php (carapp and carapp2)

--- carapp2/db.php
+++ carapp2/db.php
@@ -1,6 +1,16 @@
- <?php
-    $mysqli = new mysqli('sql111.infinityfree.com', 'if0_38310603', 'uBHu8fW6r5', 'if0_38310603_cars' );
+<?php
+try {
+    $host = $_SERVER['HTTP_HOST'];
+    if (str_contains($host,'infinityfree'))
+        $mysqli = new mysqli('sql111.infinityfree.com', 'if0_38310603', 'uBHu8fW6r5', 'if0_38310603_cars' );
+    elseif (str_contains($host,'localhost')) {
+        $mysqli = new mysqli('localhost','root','');
+        $mysqli->select_db('Cars');
+    }
     if (mysqli_connect_errno())
         die("Connect failed: %s\n".mysqli_connect_error());
-    //else echo 'Connected to mySQL<br>';
+    //echo 'Connected successfully to mySQL.<br>'; 
+} catch (Throwable $e) {
+    echo $e;
+}
 ?>
